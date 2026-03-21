5caf67431ce7f497a622fa7bcb00a1e0a204793a
yadhvi13 <yadhvi.ch@gmail.com>
2026-03-21 19:12:27 +0530

    main

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,16 +1,16 @@
-// import axios from "axios";
-
-// export const axiosInstance = axios.create({
-//   baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
-//   withCredentials: true,
-// });
-
-
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development"
-    ? "http://localhost:3000/api"
-    : `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
   withCredentials: true,
 });
+
+
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL: import.meta.env.MODE === "development"
+//     ? "http://localhost:3000/api"
+//     : `${import.meta.env.VITE_API_URL}/api`,
+//   withCredentials: true,
+// });
