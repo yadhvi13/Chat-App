02027ec4bcded23f8bd0e0e5f8fa3cfa8af2efac
yadhvi13 <yadhvi.ch@gmail.com>
2026-03-21 19:27:23 +0530

    main

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
+// export const axiosInstance = axios.create({
+//   baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+//   withCredentials: true,
+// });
+
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api"
+    : import.meta.env.VITE_API_URL;
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
 
-
-// import axios from "axios";
-
-// export const axiosInstance = axios.create({
-//   baseURL: import.meta.env.MODE === "development"
-//     ? "http://localhost:3000/api"
-//     : `${import.meta.env.VITE_API_URL}/api`,
-//   withCredentials: true,
-// });
